ae157ec4b21de70ed40d24e20bf23b7315dd1867
dkbrookie <matthew@dkbinnovative.net>
2018-11-13 13:53:48 -0600

    Update autoApplySlackDark.ps1

--- autoApplySlackDark.ps1
+++ autoApplySlackDark.ps1
@@ -11,19 +11,11 @@ If(!(Test-Path "$slackDir\ssb-interop.js.bak" -PathType Leaf)) {
 
 $slackContent = Get-Content -Path $slackFile
 If($slackContent -like "*https://raw.githubusercontent.com/dkbrookie/slackDarkmode/master/darkmode.css*") {
-  Write-Output "You already have the dark theme applied!"
+  Write-Warning "You already have the dark theme applied! Make sure to paste the exact text in a message to yourself on Slack, then click the 'Switch sidebar theme' button that appears."
+  Write-Output  "#363636,#444A47,#8c8c8c,#FEFEFE,#434745,#FEFEFE,#99D04A,#DB6668"
 } Else {
-  Write-Output "
-document.addEventListener('DOMContentLoaded', function () {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/dkbrookie/slackDarkmode/master/darkmode.css',
-        success: function (css) {
-            $("<style></style>").appendTo('head').html(css);
-        }
-    });
-});" | Out-File $slackFile -Append -Encoding ASCII
-
-  Stop-Process -Name slack -Force
+  "document.addEventListener('DOMContentLoaded', function () {$.ajax({url: 'https://raw.githubusercontent.com/dkbrookie/slackDarkmode/master/darkmode.css', success: function (css) {`$(""<style></style>"").appendTo('head').html(css);}});});" | Out-File $slackFile -Append -Encoding ASCII
+  Stop-Process -Name slack -Force -EA 0 | Out-Null
   &"C:\Users\$env:UserName\AppData\Local\slack\slack.exe"
   Write-Warning "Paste the exact text in a message to yourself on Slack, then click the 'Switch sidebar theme' button that appears."
   Write-Output  "#363636,#444A47,#8c8c8c,#FEFEFE,#434745,#FEFEFE,#99D04A,#DB6668"
